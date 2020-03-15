2289ed9536daf26fa7c796f51c40c01e96cf346a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-03-15 18:24:56 -0300

    add catch error send image

--- controllers/generalStats.js
+++ controllers/generalStats.js
@@ -3,12 +3,16 @@ const { API_URL } = require('../config/keys');
 
 module.exports = {
   getStats: async (req, res) => {
-  const { data } = await axios.get(API_URL)
-  const { confirmed, recovered, deaths } = data;
+  const { data } = await axios.get(API_URL).catch(err =>{ return res.status(404).json({ message:"Error, try again!", err }) });
+  if(!data) {
+    return;
+  }
+  const { confirmed, recovered, deaths, image } = data;
   const stats = {
     confirmed: confirmed.value,
     recovered: recovered.value,
-    deaths: deaths.value
+    deaths: deaths.value,
+    image
   }
   res.status(200).json({ stats });
   }
