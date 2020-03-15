58a973330e730cfb0ee7d2a0d09f57a2910b9436
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-03-14 22:50:44 -0300

    use axios to fetch general stats

--- controllers/generalStats.js
+++ controllers/generalStats.js
@@ -1,5 +1,15 @@
+const axios = require('axios');
+const { API_URL } = require('../config/keys');
+
 module.exports = {
-  getStats: (req, res) => {
-    res.status(200).json({stats: "OK"});
+  getStats: async (req, res) => {
+  const { data } = await axios.get(API_URL)
+  const { confirmed, recovered, deaths } = data;
+  const stats = {
+    confirmed: confirmed.value,
+    recovered: recovered.value,
+    deaths: deaths.value
+  }
+  res.status(200).json({ stats });
   }
 }
