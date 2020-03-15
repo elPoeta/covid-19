8b984c307126225cd052ad92985097c42b56d7d5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-03-15 18:37:53 -0300

    add get all countries name and iso3

--- controllers/generalStats.js
+++ controllers/generalStats.js
@@ -15,5 +15,14 @@ module.exports = {
     image
   }
   res.status(200).json({ stats });
-  }
+  },
+
+  getCountries: async (req, res) => {
+    const { data } = await axios.get(`${API_URL}/countries`).catch(err =>{ return res.status(404).json({ message:"Error, try again!", err }) });
+    if(!data) {
+      return;
+    }
+    
+    res.status(200).json({ allCountries: data });
+    }
 }
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getStats } = require('../controllers/generalStats');
+const { getStats, getCountries } = require('../controllers/generalStats');
 
 router.get('/stats', getStats);
+router.get('/countries', getCountries);
 
 module.exports = router;
